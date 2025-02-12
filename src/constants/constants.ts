0eac421d4609010ae5641e3f5fd6c2fbef8dd976
cornchiplick <ilsunkiss@gmail.com>
2025-02-13 02:04:32 +0900

    Refact: 상수 추가

---
+++ src/constants/constants.ts
@@ -0,0 +1,11 @@
+export const URL = {
+  HOME: "/",
+  RECORD: "/record",
+  STATS: "/stats",
+};
+
+export const Constants = {
+  LOTTO_PRICE_PER_GAME: 1000,
+  LOTTO_WINNING_NUMBER_COUNT: 6,
+  LOTTO_MAX_NUMBER: 45,
+};
